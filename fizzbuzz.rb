e79087a64bf169c72b818398e8e4ca6c29717422
Nadia Lee <miss.nadia.lee@gmail.com>
2017-09-30 20:25:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,13 @@
 def fizzbuzz(x)
   if x % 3 == 0
     x = "Fizz"
-  elsif (x % 3 == 0) && (x % 5 == 0)
-    x = "FizzBuzz"
   elsif x % 5 == 0
     x = "Buzz"
+    if (x % 3 == 0) && (x % 5 == 0)
+      x = "FizzBuzz"
   else ((x % 3 == 0) || (x % 5 == 0)) == false
     x = nil
+    end
   end
 end
 
